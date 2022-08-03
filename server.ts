4f0bc5d92d609961e89bdf8d778e17b2311e4df3
WU19-1 <wahyu.mai@gmail.com>
2022-08-03 17:08:13 +0700

    init

---
+++ server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import path from 'path'
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, 'build/')));
+
+app.get('*', (req, res)=>{
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+})
+
+app.listen(5000, ()=>{
+  console.log('server started')
+})
